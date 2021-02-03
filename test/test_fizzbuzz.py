4a3cfc4126b4bf29f82fb98eda045e6d401eb09b
Jigar Mehta <hello@jigarius.com>
2021-02-03 18:53:55 -0500

    Create tests for _Number.is_* methods

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -27,15 +27,27 @@ class TestNumber(unittest.TestCase):
         """_Number stringifies as 'fizz' when divisible by 3"""
         for i in [3, 6, 333]:
             n = fizzbuzz._Number(i)
-            self.assertTrue(n.is_fizz())
             self.assertEqual(str(n), 'fizz')
+
+    def test_is_fizz(self):
+        """_Number.is_fizz()"""
+        self.assertTrue(fizzbuzz._Number(0).is_fizz())
+        self.assertTrue(fizzbuzz._Number(3).is_fizz())
+        self.assertTrue(fizzbuzz._Number(15).is_fizz())
+        self.assertFalse(fizzbuzz._Number(5).is_fizz())
 
     def test_buzz_number(self):
         """_Number stringifies as 'buzz' when divisible by 3"""
         for i in [5, 10, 500]:
             n = fizzbuzz._Number(i)
-            self.assertTrue(n.is_buzz())
             self.assertEqual(str(n), 'buzz')
+
+    def test_is_buzz(self):
+        """_Number.is_buzz()"""
+        self.assertTrue(fizzbuzz._Number(0).is_buzz())
+        self.assertTrue(fizzbuzz._Number(5).is_buzz())
+        self.assertTrue(fizzbuzz._Number(15).is_buzz())
+        self.assertFalse(fizzbuzz._Number(3).is_buzz())
 
     def test_fizzbuzz_number(self):
         """_Number stringifies as 'fizzbuzz' when divisible by 3 and 5"""
@@ -43,6 +55,13 @@ class TestNumber(unittest.TestCase):
             n = fizzbuzz._Number(i)
             self.assertTrue(n.is_fizzbuzz())
             self.assertEqual(str(n), 'fizzbuzz')
+
+    def test_is_fizzbuzz(self):
+        """_Number.is_fizzbuzz()"""
+        self.assertTrue(fizzbuzz._Number(0).is_fizzbuzz())
+        self.assertTrue(fizzbuzz._Number(15).is_fizzbuzz())
+        self.assertFalse(fizzbuzz._Number(5).is_fizzbuzz())
+        self.assertFalse(fizzbuzz._Number(3).is_fizzbuzz())
 
     def test_non_fizzbuzz_number(self):
         """_Number stringifies as the number when not divisible by 3 nor 5"""
